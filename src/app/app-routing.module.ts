3d94f688a691225f4341b8f80a6a61ecc30c4b2e
Gantedavamsikrishna <vamsiganteda@gmail.com>
2024-06-26 21:25:03 +0530

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { OrdersComponent } from './orders/orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'products', component: ProductsComponent },
+  { path: 'orders', component: OrdersComponent },
+  { path: '', redirectTo: '/products', pathMatch: 'full' }, // Default route
+  { path: '**', redirectTo: '/products' }, // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
