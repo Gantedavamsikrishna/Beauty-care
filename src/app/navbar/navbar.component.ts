f698fd1757cc38557f588c6f4d56fc9dac5a99ac
Gantedavamsikrishna <vamsiganteda@gmail.com>
2024-08-09 23:12:44 +0530

    add responsive

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  @ViewChild('navbarNav', { static: false })
+  navbarNav!: ElementRef;
 
+  closeNav() {
+    if (this.navbarNav.nativeElement.classList.contains('show')) {
+      this.navbarNav.nativeElement.classList.remove('show');
+    }
+  }
 }
--- src/app/newstore/newstore.component.ts
+++ src/app/newstore/newstore.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-newstore',
   templateUrl: './newstore.component.html',
-  styleUrls: ['./newstore.component.css']
+  styleUrls: ['./newstore.component.css'],
 })
-export class NewstoreComponent {
-
-}
+export class NewstoreComponent {}
