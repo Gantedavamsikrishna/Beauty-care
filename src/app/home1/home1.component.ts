6122093290a33f581a489f03fb399803fb7d8108
Gantedavamsikrishna <vamsiganteda@gmail.com>
2024-08-09 23:25:19 +0530

    transation

--- src/app/home1/home1.component.ts
+++ src/app/home1/home1.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 
 @Component({
   selector: 'app-home1',
   templateUrl: './home1.component.html',
-  styleUrls: ['./home1.component.css']
+  styleUrls: ['./home1.component.css'],
 })
-export class Home1Component {
+export class Home1Component implements AfterViewInit {
+  @ViewChildren('content') contentElements!: QueryList<ElementRef>;
 
+  observer!: IntersectionObserver;
+
+  ngAfterViewInit(): void {
+    this.observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            this.observer.unobserve(entry.target);
+          }
+        });
+      },
+      {
+        threshold: 0.1, // Adjust the threshold as needed
+      }
+    );
+
+    this.contentElements.forEach((content) => {
+      this.observer.observe(content.nativeElement);
+    });
+  }
 }
